915a45eb6acb2cecce764c0ad50f2ce7b022f88a
Ali <45623453+AliEasy@users.noreply.github.com>
2023-02-11 21:39:55 +0330

    remove unnecessary class from general_datetime_picker.dart

--- lib/general_datetime_picker.dart
+++ lib/general_datetime_picker.dart
@@ -2,9 +2,3 @@ library general_datetime_picker;
 
 export 'package:general_datetime_picker/src/widgets/vertical_date_picker.dart';
 export 'package:general_datetime_picker/src/models/configuration.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
