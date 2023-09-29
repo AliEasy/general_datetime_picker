4ac29f8928a7cc787dd8b133b4173ee88f2ae7ca
Ali <45623453+AliEasy@users.noreply.github.com>
2023-09-29 18:52:20 +0330

    Remove Extra code

--- lib/general_datetime_picker.dart
+++ lib/general_datetime_picker.dart
@@ -3,9 +3,4 @@ library general_datetime_picker;
 export 'package:general_datetime_picker/src/widgets/vertical_date_picker.dart';
 export 'package:general_datetime_picker/src/models/configuration.dart';
 export 'package:general_datetime_picker/src/enums/date_type.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:general_datetime_picker/src/enums/separator_type.dart';
